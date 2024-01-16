691cb0ceb22c759f19a95b5a6c5d26e3d32155d9
Hallvard-Benan <hallvard.benan@gmail.com>
2024-01-16 14:01:44 +0100

    feat: post cards

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import supabase from "../../supabaseClient";
+import { MdDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
 
 function Posts() {
   const [posts, setPosts] = useState([]);
@@ -99,7 +101,10 @@ function Posts() {
 
   useEffect(() => {
     async function fetchPosts() {
-      const { data, error } = await supabase.from("post").select("*");
+      const { data, error } = await supabase
+        .from("post")
+        .select("*")
+        .order("created_at", { ascending: false });
       if (error) {
         setError(error);
       }
@@ -111,126 +116,168 @@ function Posts() {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div className="grid gap-4 grid-cols-4">
+    <div className="grid w-[calc(min(100vw-20px,1050px))] mx-auto border-b-2 border-x-2  ">
       {posts.map((post, index) => (
-        <div key={index} className="border-2">
-          <h2>{post.title}</h2>
-          <p>{post.description}</p>
-          {user === post.user && (
-            <div className="flex justify-between">
-              <button
-                onClick={() => handleDeletePost(post.id, post.user)}
-                className="border-2 p-2 border-red-500 text-red-500"
-              >
-                Delete
-              </button>
-              <button
-                onClick={() => openModal(post.id)}
-                className="border-2 p-2 border-blue-500 text-blue-500"
-              >
-                Edit
-              </button>
-            </div>
-          )}
-
-          <dialog
-            ref={(el) => (modalRefs.current[post.id] = el)}
-            className="bg-white p-4"
+        <div
+          key={index}
+          className={`border-t-2 py-4 px-6 md:flex gap-4 ${
+            index % 2 === 0 && "flex-row-reverse"
+          }`}
+        >
+          <div
+            className={`flex flex-col justify-between flex-1 ${
+              index % 2 === 0 && "text-end items-end"
+            }`}
           >
-            <form
-              onSubmit={(e) => handleUpdate(e, post.id)}
-              className="grid grid-cols-2 gap-4"
+            <div className="grid gap-4">
+              <div className="">
+                <h2 className="text-3xl font-medium">{post.title}</h2>
+                <div
+                  className={` flex ${index % 2 === 0 && "flex-row-reverse"}`}
+                >
+                  <p className="border-2 w-min px-2 text-sm mt-1">
+                    {post.genre}
+                  </p>
+                </div>
+              </div>
+              <p className="text-sm">Postet {formatDate(post.created_at)}</p>
+
+              <p className="text-sm">{post.description}</p>
+            </div>
+            <div
+              className={`flex gap-2 ${
+                index % 2 === 0 && "flex-row-reverse"
+              } items-end`}
             >
-              <div>
-                <label htmlFor="title" className="block">
-                  Title
-                </label>
-                <input
-                  type="text"
-                  defaultValue={post.title}
-                  id="title"
-                  name="title"
-                  placeholder="title"
-                  className="border-2"
-                />
-              </div>
-              <div>
-                <label htmlFor="description" className="block">
-                  Description
-                </label>
-                <input
-                  type="text"
-                  id="description"
-                  defaultValue={post.description}
-                  name="description"
-                  placeholder="Description"
-                  className="border-2"
-                />
-              </div>
-              <div>
-                <label htmlFor="genre" className="block">
-                  Genre
-                </label>
-                <input
-                  type="text"
-                  id="genre"
-                  defaultValue={post.genre}
-                  name="genre"
-                  placeholder="Genre"
-                  className="border-2"
-                />
-              </div>
-              <div>
-                <label htmlFor="thumbnail" className="block">
-                  Thumbnail URL
-                </label>
-                <input
-                  type="text"
-                  id="thumbnail"
-                  defaultValue={post.thumbnail}
-                  name="thumbnail"
-                  placeholder="thumbnail"
-                  className="border-2"
-                />
-              </div>
-              <div>
-                <label htmlFor="media" className="block">
-                  Media URL
-                </label>
-                <input
-                  type="text"
-                  id="media"
-                  name="media"
-                  defaultValue={post.media}
-                  placeholder="media"
-                  className="border-2"
-                />
-              </div>
-              <div>
-                <label htmlFor="ends" className="block">
-                  End Date
-                </label>
-                <input
-                  type="date"
-                  id="ends_at"
-                  name="ends_at"
-                  defaultValue={formatDate(post.ends_at)}
-                  placeholder="ends_at"
-                  className="border-2"
-                />
-              </div>
-              <button type="submit" className="bg-green-500 col-span-2">
-                Submit
-              </button>
-              <button
-                type="button"
-                onClick={() => closeEditModal(post.id)}
-                className="close-modal"
+              <a href="#" className="underline text-lg">
+                {"Se Prosjektet ->"}
+              </a>
+              {user === post.user && (
+                <div className="flex">
+                  <button
+                    onClick={() => handleDeletePost(post.id, post.user)}
+                    className=" p-2 text-red-500 text-2xl"
+                  >
+                    <MdDelete />
+                  </button>
+                  <button
+                    onClick={() => openModal(post.id)}
+                    className="p-2 text-blue-500 text-2xl"
+                  >
+                    <FaEdit />
+                  </button>
+                </div>
+              )}
+            </div>
+            <dialog
+              ref={(el) => (modalRefs.current[post.id] = el)}
+              className="bg-white p-4"
+            >
+              <form
+                onSubmit={(e) => handleUpdate(e, post.id)}
+                className="grid grid-cols-2 gap-4"
               >
-                Close
-              </button>
-            </form>
-          </dialog>
+                <div>
+                  <label htmlFor="title" className="block">
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    defaultValue={post.title}
+                    id="title"
+                    name="title"
+                    placeholder="title"
+                    className="border-2"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="description" className="block">
+                    Description
+                  </label>
+                  <input
+                    type="text"
+                    id="description"
+                    defaultValue={post.description}
+                    name="description"
+                    placeholder="Description"
+                    className="border-2"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="genre" className="block">
+                    Genre
+                  </label>
+                  <input
+                    type="text"
+                    id="genre"
+                    defaultValue={post.genre}
+                    name="genre"
+                    placeholder="Genre"
+                    className="border-2"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="thumbnail" className="block">
+                    Thumbnail URL
+                  </label>
+                  <input
+                    type="text"
+                    id="thumbnail"
+                    defaultValue={post.thumbnail}
+                    name="thumbnail"
+                    placeholder="thumbnail"
+                    className="border-2"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="media" className="block">
+                    Media URL
+                  </label>
+                  <input
+                    type="text"
+                    id="media"
+                    name="media"
+                    defaultValue={post.media}
+                    placeholder="media"
+                    className="border-2"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="ends" className="block">
+                    End Date
+                  </label>
+                  <input
+                    type="date"
+                    id="ends_at"
+                    name="ends_at"
+                    defaultValue={formatDate(post.ends_at)}
+                    placeholder="ends_at"
+                    className="border-2"
+                  />
+                </div>
+                <button type="submit" className="bg-green-500 col-span-2">
+                  Submit
+                </button>
+                <button
+                  type="button"
+                  onClick={() => closeEditModal(post.id)}
+                  className="close-modal"
+                >
+                  Close
+                </button>
+              </form>
+            </dialog>
+          </div>
+          <div className="flex-1">
+            <img
+              src={
+                post.thumbnail
+                  ? post.thumbnail
+                  : "https://images.squarespace-cdn.com/content/v1/614b7b9bb907e533422f0fa4/1632345033501-2NH1SZ5U86NSJDGNS6X3/image-asset.jpeg"
+              }
+              alt=""
+            />
+          </div>
         </div>
       ))}
     </div>
