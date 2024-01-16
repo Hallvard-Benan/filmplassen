8e36c059589f9a1fd7660bf557aaf76ff2d77786
PetterMartin <petter.aa@icloud.com>
2024-01-16 15:08:03 +0100

    no createlisting in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { useEffect, useState } from "react";
 import Posts from "./components/posts";
 import supabase from "./supabaseClient";
 import PostForm from "./components/Create";
-import CreateListing from "./components/creatlisting/CreateListing";
 
 const handleLogout = () => {
   window.localStorage.clear();
@@ -37,9 +36,6 @@ export default function App() {
           appearance={{ theme: ThemeSupa }}
           providers={["google"]}
         />
-        <div className="relative">
-          <CreateListing />
-        </div>
       </div>
     );
   } else {
