099a619d526f20141f360ba36cd75a022cf54040
PetterMartin <petter.aa@icloud.com>
2024-01-19 15:26:41 +0100

    animations to settings button added

--- src/components/buttons/Settings.jsx
+++ src/components/buttons/Settings.jsx
@@ -34,19 +34,49 @@ export default function SettingsButton() {
   const menuStyle = {
     position: "absolute",
     left: isClicked ? "4rem" : "0",
-    top: "0",
+    top: "0.7rem",
     transition: `left 0.3s ease-out, opacity 0.5s ${
       isClicked ? "0.1s" : "-0.2s"
     } ease`, // Adjust the delay for visibility
     opacity: isClicked ? 1 : 0,
     pointerEvents: isClicked ? "auto" : "none",
   };
+  const menuStyle2 = {
+    position: "absolute",
+    left: isClicked ? "4rem" : "0",
+    top: "3rem",
+    transition: `left 0.6s ease-out, opacity 0.5s ${
+      isClicked ? "0.3s" : "0.1s"
+    } ease`, // Adjust the delay for visibility
+    opacity: isClicked ? 1 : 0,
+    pointerEvents: isClicked ? "auto" : "none",
+  };
+  const menuStyle3 = {
+    position: "absolute",
+    left: isClicked ? "4rem" : "0",
+    top: "6.3rem",
+    transition: `left 0.9s ease-out, opacity 0.5s ${
+      isClicked ? "0.6s" : "0.2s"
+    } ease`, // Adjust the delay for visibility
+    opacity: isClicked ? 1 : 0,
+    pointerEvents: isClicked ? "auto" : "none",
+  };
+  const menuStyle4 = {
+    position: "absolute",
+    left: isClicked ? "4rem" : "0",
+    top: "9.7rem",
+    transition: `left 1.2s ease-out, opacity 0.5s ${
+      isClicked ? "0.9s" : "0.3s"
+    } ease`, // Adjust the delay for visibility
+    opacity: isClicked ? 1 : 0,
+    pointerEvents: isClicked ? "auto" : "none",
+  };
 
   return (
-    <div className="flex items-center">
+    <div className="flex items-center z-10">
       <div className="relative">
         <button
-          className="bg-transparent border-2 border-white text-white font-bold rounded-full h-12 w-12"
+          className="bg-transparent border-2 text-white font-bold rounded-full h-12 w-12"
           onMouseEnter={handleHover}
           onMouseLeave={handleMouseLeave}
           onClick={handleClick}
@@ -66,20 +96,22 @@ export default function SettingsButton() {
             ></div>
           </div>
         </button>
-        <div style={menuStyle}>
+        <div>
+          <div style={menuStyle}>
           <Link
             to={"/"}
             className="w-min border-2 rounded-full border-white py-1 px-6 text-2xl tracking-wider"
           >
             Hjem
           </Link>
-          <div className="w-min border-2 rounded-full border-white py-1 px-6 text-2xl tracking-wider mt-2">
+          </div>
+          <div style={menuStyle2} className="w-min border-2 rounded-full border-white py-1 px-6 text-2xl tracking-wider mt-2">
             Kortfilmer
           </div>
-          <div className="w-min border-2 rounded-full border-white py-1 px-6 text-2xl tracking-wider mt-2">
+          <div style={menuStyle3}  className="w-min border-2 rounded-full border-white py-1 px-6 text-2xl tracking-wider mt-2">
             Filmer
           </div>
-          <div className="w-min border-2 rounded-full border-white py-1 px-6 text-2xl tracking-wider mt-2">
+          <div style={menuStyle4}  className="w-min border-2 rounded-full border-white py-1 px-6 text-2xl tracking-wider mt-2">
             Musikkvideo
           </div>
         </div>
