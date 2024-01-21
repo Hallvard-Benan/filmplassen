a8e5a6e90c8d6e65dfed158e91943d823164d250
PetterMartin <petter.aa@icloud.com>
2024-01-21 17:30:17 +0100

    settings and profile buttons now stick

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -24,6 +24,9 @@ function HomePage() {
   if (!session) {
     return (
       <div>
+        <div className="my-8 flex justify-center w-full">
+          <h1 className="text-3xl font-thin border-b pb-2 px-6">Velkommen til Filmplassen</h1>
+        </div>
         <div className="relative w-[calc(min(100vw-20px,1050px))] h-[550px] mx-auto">
           <ImageSlider />
         </div>
